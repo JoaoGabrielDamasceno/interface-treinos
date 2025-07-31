cb37320aba56d9283fd071804689553ba2ff6f45
João Gabriel <joao.gabriel@example.com>
2025-07-30 23:03:34 -0300

    ajuste add series

--- src/interface_treino/views.cljs
+++ src/interface_treino/views.cljs
@@ -1,14 +1,11 @@
 (ns interface-treino.views
   (:require
-   [re-com.core :as re-com :refer [at]]
    [reagent.core :as r]))
 
 (def estado (r/atom :inicial))
 (def nome-exercicio (r/atom ""))
 (def data (r/atom ""))
-(def serie (r/atom ""))
-(def repeticao (r/atom ""))
-(def peso (r/atom ""))
+(def series (r/atom [{:serie "" :repeticao "" :peso ""}]))
 
 (defn main-panel []
   [:div {:style {:background-color "white"
@@ -152,60 +149,80 @@
                          :border-radius "3px"
                          :font-size "14px"}}]]
        
-       ; Campos de série em linha
+       ; Lista de séries
+       [:div {:style {:display "flex"
+                      :flex-direction "column"
+                      :gap "10px"}}
+        (map-indexed 
+         (fn [idx serie-data]
+           ^{:key idx}
+           [:div {:style {:display "flex"
+                          :gap "10px"
+                          :align-items "end"}}
+            ; Série
+            [:div {:style {:flex "0 0 60px"}}
+             [:label {:style {:display "block" 
+                              :margin-bottom "5px"
+                              :font-size "12px"
+                              :color "black"}} 
+              "Série"]
+             [:input {:type "text"
+                      :value (:serie serie-data)
+                      :on-change #(swap! series assoc-in [idx :serie] (-> % .-target .-value))
+                      :style {:width "100%"
+                              :padding "6px"
+                              :border "1px solid #000"
+                              :border-radius "3px"
+                              :font-size "12px"}}]]
+            
+            ; Repetições
+            [:div {:style {:flex "0 0 80px"}}
+             [:label {:style {:display "block" 
+                              :margin-bottom "5px"
+                              :font-size "12px"
+                              :color "black"}} 
+              "Repetições"]
+             [:input {:type "text"
+                      :value (:repeticao serie-data)
+                      :on-change #(swap! series assoc-in [idx :repeticao] (-> % .-target .-value))
+                      :style {:width "100%"
+                              :padding "6px"
+                              :border "1px solid #000"
+                              :border-radius "3px"
+                              :font-size "12px"}}]]
+            
+            ; Peso
+            [:div {:style {:flex "0 0 70px"}}
+             [:label {:style {:display "block" 
+                              :margin-bottom "5px"
+                              :font-size "12px"
+                              :color "black"}} 
+              "Peso"]
+             [:input {:type "text"
+                      :value (:peso serie-data)
+                      :on-change #(swap! series assoc-in [idx :peso] (-> % .-target .-value))
+                      :style {:width "100%"
+                              :padding "6px"
+                              :border "1px solid #000"
+                              :border-radius "3px"
+                              :font-size "12px"}}]]
+            
+            ; Botão remover série (só aparece se tiver mais de 1)
+            (when (> (count @series) 1)
+              [:button {:style {:padding "6px 8px"
+                               :border "1px solid #ff0000"
+                               :border-radius "3px"
+                               :background-color "white"
+                               :color "red"
+                               :cursor "pointer"
+                               :font-size "12px"}
+                       :on-click #(swap! series (fn [s] (vec (concat (take idx s) (drop (inc idx) s)))))}
+               "X"])])
+         @series)]
+       
+       ; Botões
        [:div {:style {:display "flex"
                       :gap "10px"
-                      :align-items "end"}}
-        ; Série
-        [:div {:style {:flex "0 0 60px"}}
-         [:label {:style {:display "block" 
-                          :margin-bottom "5px"
-                          :font-size "12px"
-                          :color "black"}} 
-          "Série"]
-         [:input {:type "text"
-                  :value @serie
-                  :on-change #(reset! serie (-> % .-target .-value))
-                  :style {:width "100%"
-                          :padding "6px"
-                          :border "1px solid #000"
-                          :border-radius "3px"
-                          :font-size "12px"}}]]
-        
-        ; Repetições
-        [:div {:style {:flex "0 0 80px"}}
-         [:label {:style {:display "block" 
-                          :margin-bottom "5px"
-                          :font-size "12px"
-                          :color "black"}} 
-          "Repetições"]
-         [:input {:type "text"
-                  :value @repeticao
-                  :on-change #(reset! repeticao (-> % .-target .-value))
-                  :style {:width "100%"
-                          :padding "6px"
-                          :border "1px solid #000"
-                          :border-radius "3px"
-                          :font-size "12px"}}]]
-        
-        ; Peso
-        [:div {:style {:flex "0 0 70px"}}
-         [:label {:style {:display "block" 
-                          :margin-bottom "5px"
-                          :font-size "12px"
-                          :color "black"}} 
-          "Peso"]
-         [:input {:type "text"
-                  :value @peso
-                  :on-change #(reset! peso (-> % .-target .-value))
-                  :style {:width "100%"
-                          :padding "6px"
-                          :border "1px solid #000"
-                          :border-radius "3px"
-                          :font-size "12px"}}]]]
-       
-       ; Botão Adicionar Série
-       [:div {:style {:display "flex"
                       :justify-content "center"
                       :margin-top "15px"}}
         [:button {:style {:padding "12px 24px"
@@ -215,10 +232,20 @@
                           :color "black"
                           :cursor "pointer"
                           :font-size "14px"}
-                  :on-click #(js/alert (str "Série adicionada!\n"
+                  :on-click #(swap! series conj {:serie "" :repeticao "" :peso ""})}
+         "Adicionar Série"]
+        
+        [:button {:style {:padding "12px 24px"
+                          :border "1px solid #000"
+                          :border-radius "5px"
+                          :background-color "#007bff"
+                          :color "white"
+                          :cursor "pointer"
+                          :font-size "14px"
+                          :font-weight "bold"}
+                  :on-click #(js/alert (str "Treino Cadastrado!\n"
                                            "Exercício: " @nome-exercicio "\n"
                                            "Data: " @data "\n"
-                                           "Série: " @serie "\n"
-                                           "Repetições: " @repeticao "\n"
-                                           "Peso: " @peso))}
-         "Adicionar Série"]]])]])
+                                           "Séries: " (count @series) "\n"
+                                           "Detalhes: " (pr-str @series)))}
+         "Cadastrar"]]])]])
